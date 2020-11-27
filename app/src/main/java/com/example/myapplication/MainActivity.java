e5ce399d0dd818b63ba4d14fa7ce9a1b5a57ff21
allen233333 <1657054849@qq.com>
2020-11-27 14:13:09 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View view){
+        TextView txtHello = findViewById(R.id.textView);
+        EditText edtTxtName = findViewById(R.id.Name);
+        txtHello.setText(edtTxtName.getText().toString());
+    }
+}
